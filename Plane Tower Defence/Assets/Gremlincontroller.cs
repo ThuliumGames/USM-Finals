19979f2be433eaaa2e49ab237c7dc7af7eb9a4df
AzureAD\EvanNorzow <LeSkree@gmail.com>
2019-04-23 09:49:05 -0400

    Enemy start.
    E

---
+++ Plane Tower Defence/Assets/Gremlincontroller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gremlincontroller : MonoBehaviour
+{
+    public Vector3 ppos;
+    public Vector3 epos;
+    public Vector3 floorppos;
+    public Transform ptrans;
+    public float dist;
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ptrans = GameObject.Find("Heli").transform;
+        ppos = new Vector3(ptrans.position.x, ptrans.position.y, ptrans.position.z);
+        floorppos = new Vector3(ptrans.position.x, this.transform.position.y, ptrans.position.z);
+        epos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.SqrMagnitude(this.transform.position - ptrans.position);
+
+        float step = Speed * Time.deltaTime;
+        floorppos = new Vector3(ptrans.position.x, this.transform.position.y, ptrans.position.z);
+
+        if (dist >= 5)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, floorppos, step);
+        }
+    }
+}
