a6ed40c9727ffbaaa6a8b963db3fbda446803210
Thulium Games <32959306+ThuliumGames@users.noreply.github.com>
2019-04-24 09:19:51 -0400

    Merge

---
+++ Plane Tower Defence/Assets/Scripts/KillThings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillThings : MonoBehaviour {
+	
+	public bool isTimed;
+	public float Health = 25;
+	
+	void Update () {
+		if (!isTimed) {
+			if (Health <= 0) {
+				Destroy(gameObject);
+			}
+		} else {
+			Invoke("Kill", Health);
+		}
+	}
+	
+	void Kill () {
+		Destroy(gameObject);
+	}
+}
